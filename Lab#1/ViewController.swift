7079d8a8552fd72c9865ceabb92f7ded13ea7c12
woox899 <woox899@gmail.com>
2023-10-23 20:46:02 +0400

    Initial Commit

---
+++ Lab#1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab#1
+//
+//  Created by Андрей Бабкин on 23.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
